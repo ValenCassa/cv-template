5fa8ea63f48ebd964baec021ac4f096e7cba7d57
Valentin Cassarino <valencassa@gmail.com>
2022-12-03 14:19:30 -0300

    Added VERCEL

--- services/index.ts
+++ services/index.ts
@@ -1,8 +1,9 @@
 import pageConfig from "pageConfig.json";
-const { hostName } = pageConfig;
 
 export const BASE_URL =
-  process.env.NODE_ENV === "production" ? hostName : "http://localhost:3000";
+  process.env.NODE_ENV === "production"
+    ? process.env.VERCEL_URL
+    : "http://localhost:3000";
 
 export const getProjects = async () => {
   const data = await fetch(`${BASE_URL}/api/projects`);
